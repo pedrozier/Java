8ed5b7fd0f15c38ee8f8ee206f54cc0d3efdfbe6
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2021-06-17 16:23:06 -0300

    Add files via upload

---
+++ IdiomaDoSistema/src/idiomadosistema/IdiomaDoSistema.java
@@ -0,0 +1,13 @@
+
+package idiomadosistema;
+
+import java.util.Locale;
+
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+         Locale loc = Locale.getDefault();
+         System.out.println(loc.toString());
+    }
+    
+}
---
+++ ResolucaoDaTela/src/resolucaodatela/ResolucaoDaTela.java
@@ -0,0 +1,16 @@
+
+package resolucaodatela;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolucaoDaTela {
+
+    public static void main(String[] args) {
+            Toolkit tk = Toolkit.getDefaultToolkit();
+    Dimension d = tk.getScreenSize();
+    System.out.println(+ d.width );
+        System.out.println(+ d.height);
+    }
+    
+}
