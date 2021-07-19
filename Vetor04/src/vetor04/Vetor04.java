27f29cd52dca9a0e5dd4318b6c0e5c3f22fd7d3c
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2021-07-19 19:45:44 -0300

    Add files via upload

---
+++ Vetor04/src/vetor04/Vetor04.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor04;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author zier
+ */
+public class Vetor04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int vet[] = {3, 7, 6, 1, 9, 4, 2};
+        for(int v:vet) {
+            System.out.println(v + " ");
+        }
+        System.out.println("");
+        int p = Arrays.binarySearch(vet, 1);
+        System.out.println("O valor esta na posicao " + p);
+    }
+
+}
